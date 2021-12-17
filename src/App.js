c205db3c3fa2eae0650995e31c37096207d4844b
Julius Robinson <julius.robinson95@yahoo.com>
2021-12-16 21:52:04 -0700

    refactored the app and completed step one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
+import React, { useState, useEffect } from "react";
+
 function App() {
+	const url = "https://api.hatchways.io/assessment/students";
+	const [students, setStudents] = useState([]);
+
+	useEffect(() => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				setStudents(
+					data.students.map((student) =>
+						Object.assign(student, { tags: [] })
+					)
+				);
+			});
+	}, []);
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-			</header>
+			{students.map((student) => (
+				<Student
+					key={`${student.firstName}${student.lastName}`}
+					data={student}
+				/>
+			))}
 		</div>
+	);
+}
+
+function Student({ data }) {
+	const averageGrades = (arrayToConvert) => {
+		let arr = arrayToConvert.map((elem) => parseInt(elem, 10));
+		return arr.reduce((a, b) => a + b, 0) / arr.length;
+	};
+	return (
+		<section className="student">
+			<div className="image">
+				<img src={data.pic} alt={`${data.firstName}${data.lastName}`} />
+			</div>
+			<div className="details">
+				<h1>
+					{data.firstName} {data.lastName}
+				</h1>
+				<p>Email: {data.email}</p>
+				<p>Company: {data.company}</p>
+				<p>Skill: {data.skill}</p>
+				<p>Average: {`${averageGrades(data.grades).toFixed(2)}%`}</p>
+			</div>
+		</section>
 	);
 }
 
